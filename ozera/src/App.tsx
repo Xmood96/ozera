9851f364658eb94b09e55efe0f65b59db8ae5636
Xmood96 <xm0d@hotmail.com>
2025-11-22 07:44:55 +0300

    first commit

---
+++ ozera/src/App.tsx
@@ -0,0 +1,17 @@
+import JewelLogo from "./assets/logoanim"
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+<div className="w-52 h-52">
+      <JewelLogo size={100} color="#C29D5" loopDuration={5} strokeWidth={8} />
+    </div>
+    </>
+  )
+}
+
+export default App
